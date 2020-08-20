c0d8b73cec2e2004b9ed4d44f29a0800d29b7afe
Kostya Polishko <kost927@gmail.com>
2020-08-20 16:22:00 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import css from './Index.module.css'
+
+
+ReactDOM.render(
+
+    <App />,
+
+  document.getElementById('root')
+);
+
